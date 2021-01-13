20e53620496e3963562dc46efd58bd25d3ce672f
Hendi Santika <hendisantika@yahoo.co.id>
2021-01-14 06:32:10 +0700

    #11 - Add uploadFile API in StorageController

--- src/main/java/com/hendisantika/springboots3demo/controller/StorageController.java
+++ src/main/java/com/hendisantika/springboots3demo/controller/StorageController.java
@@ -2,8 +2,13 @@ package com.hendisantika.springboots3demo.controller;
 
 import com.hendisantika.springboots3demo.service.StorageService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 /**
  * Created by IntelliJ IDEA.
@@ -20,4 +25,9 @@ public class StorageController {
 
     @Autowired
     private StorageService service;
+
+    @PostMapping("/upload")
+    public ResponseEntity<String> uploadFile(@RequestParam(value = "file") MultipartFile file) {
+        return new ResponseEntity<>(service.uploadFile(file), HttpStatus.OK);
+    }
 }
