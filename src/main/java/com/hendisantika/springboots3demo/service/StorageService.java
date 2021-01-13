6806df7f69da224154728a8e4dd65b085c9e9a0a
Hendi Santika <hendisantika@yahoo.co.id>
2021-01-14 06:29:51 +0700

    #8 - Add convertMultiPartFileToFile in StorageService

--- src/main/java/com/hendisantika/springboots3demo/service/StorageService.java
+++ src/main/java/com/hendisantika/springboots3demo/service/StorageService.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 /**
@@ -58,5 +59,14 @@ public class StorageService {
         return fileName + " removed ...";
     }
 
+    private File convertMultiPartFileToFile(MultipartFile file) {
+        File convertedFile = new File(file.getOriginalFilename());
+        try (FileOutputStream fos = new FileOutputStream(convertedFile)) {
+            fos.write(file.getBytes());
+        } catch (IOException e) {
+            log.error("Error converting multipartFile to file", e);
+        }
+        return convertedFile;
+    }
 
 }
