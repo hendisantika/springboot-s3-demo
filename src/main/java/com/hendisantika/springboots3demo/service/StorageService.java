1c86ac7b86512befbb03fc8156a6eb83c9e7d6f8
Hendi Santika <hendisantika@yahoo.co.id>
2021-01-14 06:28:42 +0700

    #5 - Add uploadFile in StorageService

--- src/main/java/com/hendisantika/springboots3demo/service/StorageService.java
+++ src/main/java/com/hendisantika/springboots3demo/service/StorageService.java
@@ -1,10 +1,14 @@
 package com.hendisantika.springboots3demo.service;
 
 import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.PutObjectRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
 
 /**
  * Created by IntelliJ IDEA.
@@ -24,4 +28,13 @@ public class StorageService {
 
     @Autowired
     private AmazonS3 s3Client;
+
+    public String uploadFile(MultipartFile file) {
+        File fileObj = convertMultiPartFileToFile(file);
+        String fileName = System.currentTimeMillis() + "_" + file.getOriginalFilename();
+        s3Client.putObject(new PutObjectRequest(bucketName, fileName, fileObj));
+        fileObj.delete();
+        return "File uploaded : " + fileName;
+    }
+
 }
