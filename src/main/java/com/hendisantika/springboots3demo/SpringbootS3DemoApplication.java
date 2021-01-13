132ecbb569bb50b3b176faec7dd6c9a0c505b8a4
Hendi Santika <hendisantika@yahoo.co.id>
2021-01-14 06:21:27 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/springboots3demo/SpringbootS3DemoApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springboots3demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootS3DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootS3DemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hendisantika/springboots3demo/SpringbootS3DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springboots3demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootS3DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
